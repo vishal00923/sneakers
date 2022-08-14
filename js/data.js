5f99e6d2a4dc5b0771b557d147a9092f5b1b51fd
Vishal Chaurasia <vishalbg.mz@gmail.com>
2022-08-14 15:56:50 +0530

    add products data

---
+++ js/data.js
@@ -0,0 +1,77 @@
+const products = [
+  {
+    id: 1,
+    title: 'Air Forge',
+    price: 119,
+    colors: [
+      {
+        code: 'black',
+        img: '../images/air.png',
+      },
+      {
+        code: 'darkblue',
+        img: '../images/air2.png',
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: 'Jordan',
+    price: 220,
+    colors: [
+      {
+        code: 'lightgray',
+        img: '../images/jordan.png',
+      },
+      {
+        code: 'green',
+        img: '../images/jordan2.png',
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: 'Blazer',
+    price: 230,
+    colors: [
+      {
+        code: 'lightgray',
+        img: '../images/blazer.png',
+      },
+      {
+        code: 'green',
+        img: '../images/blazer2.png',
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: 'Crater',
+    price: 245,
+    colors: [
+      {
+        code: 'black',
+        img: '../images/crater.png',
+      },
+      {
+        code: 'lightgray',
+        img: '../images/crater2.png',
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: 'Hippie',
+    price: 250,
+    colors: [
+      {
+        code: 'gray',
+        img: '../images/hippie.png',
+      },
+      {
+        code: 'black',
+        img: '../images/hippie2.png',
+      },
+    ],
+  },
+];
