c3ef12b2f3c7a294556ca029159d333ab49e0330
Vishal Chaurasia <vishalbg.mz@gmail.com>
2022-08-16 00:10:30 +0530

    add payment functionality

--- js/main.js
+++ js/main.js
@@ -47,3 +47,15 @@ currentProductSizes.forEach((item) => {
     item.style.color = '#f0f8ff';
   });
 });
+
+const productButton = document.querySelector('.product-button');
+const payment = document.querySelector('.payment');
+const close = document.querySelector('.close');
+
+productButton.addEventListener('click', () => {
+  payment.style.display = 'flex';
+});
+
+close.addEventListener('click', () => {
+  payment.style.display = 'none';
+});
